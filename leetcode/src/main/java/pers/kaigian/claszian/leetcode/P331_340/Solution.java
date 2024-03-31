d1f3f6ecc666e1794fdf861f558d2c168b64faaa
kaigian <kaigian@foxmail.com>
2024-03-31 10:04:05 +0800

    leetcode-331

---
+++ leetcode/src/main/java/pers/kaigian/claszian/leetcode/P331_340/Solution.java
@@ -0,0 +1,30 @@
+package pers.kaigian.claszian.leetcode.P331_340;
+
+import java.util.Stack;
+
+/**
+ * @author kaigian
+ */
+public class Solution {
+    /**
+     * P331:给出一个二叉树的前序，验证其是否合法
+     */
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        for (String node : nodes) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            int cur = stack.pop() - 1;
+            if (cur > 0) {
+                stack.push(cur);
+            }
+            if (!"#".equals(node)) {
+                stack.push(2);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
