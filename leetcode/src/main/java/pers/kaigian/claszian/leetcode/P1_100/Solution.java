03b8a997b63b380ad6a5c76872c6cc9b7477eb9d
kaigian <kaigian@foxmail.com>
2026-02-09 17:43:17 +0800

    leetcode-34

--- leetcode/src/main/java/pers/kaigian/claszian/leetcode/P1_100/Solution.java
+++ leetcode/src/main/java/pers/kaigian/claszian/leetcode/P1_100/Solution.java
@@ -2,6 +2,37 @@ package pers.kaigian.claszian.leetcode.P1_100;
 
 public class Solution {
     /**
+     * P34:在排序数组中查找元素的第一个和最后一个位置
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, start = nums.length, end = -1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] >= target) {
+                start = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (start > nums.length - 1 || nums[start] != target) {
+            return new int[] { -1, -1 };
+        }
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] <= target) {
+                end = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return new int[] { start, end };
+    }
+
+    /**
      * P35:搜索插入位置
      */
     public int searchInsert(int[] nums, int target) {
