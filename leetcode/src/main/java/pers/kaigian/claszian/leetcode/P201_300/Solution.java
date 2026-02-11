ae3ed6b53c3d4c7f0d8987f3def1e000cfb476e3
kaigian <kaigian@foxmail.com>
2026-02-11 10:20:13 +0800

    leetcode-209

--- leetcode/src/main/java/pers/kaigian/claszian/leetcode/P201_300/Solution.java
+++ leetcode/src/main/java/pers/kaigian/claszian/leetcode/P201_300/Solution.java
@@ -2,6 +2,33 @@ package pers.kaigian.claszian.leetcode.P201_300;
 
 public class Solution {
     /**
+     * P209:长度最小的子数组
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0, right = 0, sum = nums[0], result = 0;
+        while (right < nums.length) {
+            if (right < left) {
+                right++;
+                continue;
+            }
+            if (sum >= target) {
+                if (result == 0) {
+                    result = right - left + 1;
+                } else {
+                    result = Math.min(result, right - left + 1);
+                }
+                sum -= nums[left++];
+                continue;
+            }
+            if (right == nums.length - 1) {
+                break;
+            }
+            sum += nums[++right];
+        }
+        return result;
+    }
+
+    /**
      * P283:移动零
      */
     public void moveZeroes(int[] nums) {
@@ -17,5 +44,11 @@ public class Solution {
             slow++;
             fast++;
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 2, 3, 1, 2, 4, 3 };
+        System.out.println(solution.minSubArrayLen(7, nums));
     }
 }
