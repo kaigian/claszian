4afaa7c465c6f1ab3d470f88d7d65f9f457cd1fd
kaigian <kaigian@foxmail.com>
2026-02-11 10:43:37 +0800

    leetcode-904

--- leetcode/src/main/java/pers/kaigian/claszian/leetcode/P901_1000/Solution.java
+++ leetcode/src/main/java/pers/kaigian/claszian/leetcode/P901_1000/Solution.java
@@ -1,6 +1,38 @@
 package pers.kaigian.claszian.leetcode.P901_1000;
 
+import java.util.*;
+
 public class Solution {
+    /**
+     * P904:水果成篮
+     */
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> cntMap = new HashMap<>();
+        int left = 0, right = 0, cnt = 0, ans = 0;
+        while (right < fruits.length) {
+            if (left > right) {
+                right++;
+                continue;
+            }
+            if (cntMap.containsKey(fruits[right]) || cntMap.keySet().size() < 2) {
+                cntMap.put(fruits[right], cntMap.getOrDefault(fruits[right], 0) + 1);
+                right++;
+                cnt++;
+                ans = Math.max(ans, cnt);
+                continue;
+            }
+            Integer tmpCnt = cntMap.get(fruits[left]);
+            if (tmpCnt == 1) {
+                cntMap.remove(fruits[left]);
+            } else {
+                cntMap.put(fruits[left], tmpCnt - 1);
+            }
+            cnt--;
+            left++;
+        }
+        return ans;
+    }
+
     /**
      * P977:有序数组的平方
      */
